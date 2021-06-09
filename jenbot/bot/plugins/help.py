6a9f534e9a07deedfb4fbf3ecd85fc81e2fbeee3
j1shnu <jishnu.prasad4@gmail.com>
2021-06-09 21:10:15 +0530

    Updated help command

--- jenbot/bot/plugins/help.py
+++ jenbot/bot/plugins/help.py
@@ -16,7 +16,8 @@ async def help(c: JenkinsBot, m: Message):
         "`/auth`   -  __This authorize a group or a particular user you're replying to.__\n"
         "`/unauth` -  __This do the oposite of `/auth`.__\n"
         "`/listauth` - __This'll show you all chats and users are authorized to use the bot.__\n"
-        "`/jobs`   -  __This'll show you the available Jenkins job in InlineKeyboard format.__\n\n"
+        "`/jobs`   -  __This'll show you the available Jenkins job in InlineKeyboard format.__\n"
+        "`/showbuild` - __This'll show you the running Builds.__\n\n"
         f"And more features are coming...{emoji.SMILING_CAT_WITH_HEART_EYES}"
     )
 
